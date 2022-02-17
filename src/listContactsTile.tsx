34e1cf6c31f1284000edbe72f5886ab92c65f38d
Dwikavindra Haryo Radithya <dwika@Dwikavindras-MacBook-Air.local>
2022-02-17 17:11:04 +0700

    Fixed trash position to the end

--- src/listContactsTile.tsx
+++ src/listContactsTile.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Contact from "./Contact";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 interface contactLists {
   contactList: Contact[];
 }
 function ListContactTile(props: contactLists) {
   return (
-    <ul>
+    <div>
       {props.contactList.map((element) => {
         return (
-          <li key={`${element}`}>
-            {element.name} {element.phoneNumber}
-          </li>
+          <div className="flex flex-row bg-gray-300 border-2 ml-5 mr-5 ">
+            <div className="flex flex-col">
+              <h2>{element.name}</h2>
+              <h3>{element.phoneNumber}</h3>
+            </div>
+            <div className="flex w-screen justify-end items-end">
+              <button className="ml-60 self-end items-end ">
+                <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+              </button>
+            </div>
+          </div>
         );
       })}
-    </ul>
+    </div>
   );
 }
 
