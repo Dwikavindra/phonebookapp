a17f3409cd77292567106034f31952333793acd8
Dwikavindra Haryo Radithya <dwika@Dwikavindras-MacBook-Air.local>
2022-02-22 14:07:34 +0700

    Added Fix Error Check in Strings

--- src/ModalAddContact.tsx
+++ src/ModalAddContact.tsx
@@ -19,13 +19,53 @@ function ModalAddContact(props: AddContactProps) {
     name: "",
     phoneNumber: "",
   };
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [showErrorInvalidPattern, setErrorInvalidPattern] =
+    useState("invisible");
+  const [showErrorNoname, setErrorNoname] = useState("invisible");
+  const [showErrorInvalidLength, setErrorInvalidLength] = useState("invisible");
+  const [errorCheckNamePass, setErrorCheckNamePass] = useState<boolean>(false);
+  const [errorCheckNumberPass, setErrorCheckNumberPass] =
+    useState<boolean>(false);
+  const [isPhoneNumberLengthPass, setPhoneNumberLengthPass] =
+    useState<boolean>(false);
   function AddContact(contact: Contact, name: string, phone: string) {
     individualContact.name = name;
     individualContact.phoneNumber = phone;
     props.setContactAdd(individualContact);
   }
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+
+  function ErrorCheckname(name: string) {
+    let nameLength: number = name.length;
+    if (nameLength > 0) {
+      setErrorCheckNamePass(true);
+      setErrorNoname("invisible");
+    } else {
+      setErrorNoname("visible");
+      setErrorCheckNamePass(false);
+    }
+  }
+  function ErrorCheckphoneNumber(phoneNumber: string) {
+    let phoneNumberLength = phoneNumber.length;
+    let regex: RegExp = /\+?([ -]?\d+)+|\(\d+\)([ -]\d+) /g;
+    let regexresult: boolean = regex.test(phoneNumber);
+    console.log(phoneNumberLength);
+
+    if (phoneNumberLength < 11 || phoneNumberLength > 15) {
+      setErrorInvalidPattern("invisible");
+      setErrorInvalidLength("visible");
+    } else {
+      setPhoneNumberLengthPass(true);
+    }
+    if (regexresult === false) {
+      setErrorInvalidLength("invisible");
+      setErrorInvalidPattern("visible");
+    }
+    if (regexresult === true && isPhoneNumberLengthPass === true) {
+      setErrorCheckNumberPass(true);
+    }
+  }
   return (
     <div
       className={`${props.showModalContactAdd} bg-black bg-opacity-50 absolute inset-0 flex justify-center w-screen `}
@@ -49,6 +89,9 @@ function ModalAddContact(props: AddContactProps) {
           value={name}
           onChange={(e) => setName(e.target.value)}
         ></input>
+        <h3 className={`${showErrorNoname} text-red-500`}>
+          Name cannot be empty
+        </h3>
         <h3 className="mt-10">Phone Number</h3>
         <input
           className="flex ml-2 mr-2 border-2 border-solid border-black"
@@ -58,13 +101,30 @@ function ModalAddContact(props: AddContactProps) {
             setPhoneNumber(e.target.value);
           }}
         ></input>
+        <h3 className={`${showErrorInvalidPattern} text-red-500`}>
+          Invalid Pattern
+        </h3>
+        <h3 className={`${showErrorInvalidLength} text-red-500`}>
+          Invalid Length must be 11 characters at least and 15 at most
+        </h3>
         <div className="flex justify-center items-center">
           <button
             className=" mt-10 flex justify-center items-center border-2 bg-blue-500 text-white rounded-lg border-transparent"
             onClick={() => {
-              AddContact(individualContact, name, phoneNumber);
-              setName("");
-              setPhoneNumber("");
+              ErrorCheckname(name);
+              ErrorCheckphoneNumber(phoneNumber);
+              if (
+                errorCheckNamePass === true &&
+                errorCheckNumberPass === true
+              ) {
+                setErrorInvalidLength("invisible");
+                setErrorInvalidPattern("invisible");
+                setErrorNoname("invisible");
+                AddContact(individualContact, name, phoneNumber);
+                setName("");
+                setPhoneNumber("");
+                props.setShowModalContactAdd("invisble");
+              }
             }}
           >
             <h3 className="mt-1  ml-3 mr-4 mb-1 text-xs"> Add Contact</h3>
--- src/ModalUpdateData.tsx
+++ src/ModalUpdateData.tsx
@@ -25,7 +25,20 @@ function ModalUpdateData(props: UpdateContactProps) {
   const [phoneNumbermodal, setPhoneNumber] = useState(props.phoneNumber);
   const [showErrorInvalidPattern, setErrorInvalidPattern] =
     useState("invisible");
+  const [showErrorNoname, setErrorNoname] = useState("invisible");
   const [showErrorInvalidLength, setErrorInvalidLength] = useState("invisible");
+  const [errorCheckNamePass, setErrorCheckNamePass] = useState<boolean>(false);
+  const [errorCheckNumberPass, setErrorCheckNumberPass] =
+    useState<boolean>(false);
+  const [isPhoneNumberLengthPass, setPhoneNumberLengthPass] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (props.showModalContactUpdate === "visible") {
+      ErrorCheckname(namemodal);
+      ErrorCheckphoneNumber(phoneNumbermodal);
+    }
+  });
   useEffect(() => {
     //useEffect use it to re render certain elements when called
     setName(props.name);
@@ -40,7 +53,7 @@ function ModalUpdateData(props: UpdateContactProps) {
   function updateContactList(id: number, name: string, phone: string) {
     let contactList: Contact[] = props.nonSearchContactList;
     contactList.find((item) => {
-      if (item.id == id) {
+      if (item.id === id) {
         item.name = name;
         item.phoneNumber = phone;
       }
@@ -52,12 +65,41 @@ function ModalUpdateData(props: UpdateContactProps) {
   function updateSearchContactList(id: number, name: string, phone: string) {
     let searchContactList: Contact[] = props.searchContactList;
     searchContactList.find((item) => {
-      if (item.id == id) {
+      if (item.id === id) {
         item.name = name;
         item.phoneNumber = phone;
       }
     });
     props.setSearchContactList(searchContactList);
+  }
+  function ErrorCheckname(name: string) {
+    let nameLength: number = name.length;
+    if (nameLength > 0) {
+      setErrorCheckNamePass(true);
+      setErrorNoname("invisible");
+    } else {
+      setErrorNoname("visible");
+      setErrorCheckNamePass(false);
+    }
+  }
+  function ErrorCheckphoneNumber(phoneNumber: string) {
+    let phoneNumberLength = phoneNumber.length;
+    let regex: RegExp = /\+?([ -]?\d+)+|\(\d+\)([ -]\d+) /g;
+    let regexresult: boolean = regex.test(phoneNumber);
+    if (regexresult === true && isPhoneNumberLengthPass === true) {
+      setErrorCheckNumberPass(true);
+    } else {
+      if (phoneNumberLength < 11 || phoneNumberLength > 15) {
+        setErrorInvalidPattern("invisible");
+        setErrorInvalidLength("visible");
+      } else {
+        setPhoneNumberLengthPass(true);
+      }
+      if (regexresult === false) {
+        setErrorInvalidLength("invisible");
+        setErrorInvalidPattern("visible");
+      }
+    }
   }
   return (
     <div
@@ -70,6 +112,9 @@ function ModalUpdateData(props: UpdateContactProps) {
             className="flex inline-text ml-60"
             onClick={() => {
               props.setShowModalContactUpdate();
+              setErrorInvalidLength("invisible");
+              setErrorInvalidPattern("invisible");
+              setErrorNoname("invisible");
             }}
           >
             <FontAwesomeIcon icon={faXmark}></FontAwesomeIcon>
@@ -85,13 +130,12 @@ function ModalUpdateData(props: UpdateContactProps) {
             setName(e.target.value);
           }}
         ></input>
-        {/* <h3 className={`${showErrorInvalidPattern} text-red-500`}>
-          Invalid Pattern
-        </h3> */}
-        {/* <h3 className={`${showErrorInvalidLength} text-red-500`}>
-          Invalid Length must be 11 characters at least and 15 at most
-        </h3> */}
+        <h3 className={`${showErrorNoname} text-red-500`}>
+          Name cannot be empty
+        </h3>
+
         <h3 className="mt-10">Phone Number</h3>
+
         <input
           key={props.id}
           className="flex ml-2 mr-2 border-2 border-solid border-black"
@@ -101,30 +145,36 @@ function ModalUpdateData(props: UpdateContactProps) {
             setPhoneNumber(e.target.value);
           }}
         ></input>
+        <h3 className={`${showErrorInvalidPattern} text-red-500`}>
+          Invalid Pattern
+        </h3>
+        <h3 className={`${showErrorInvalidLength} text-red-500`}>
+          Invalid Length must be 11 characters at least and 15 at most
+        </h3>
         <div className="flex justify-center items-center">
           <button
             className=" mt-10 flex justify-center items-center border-2 bg-blue-500 text-white rounded-lg border-transparent"
             onClick={() => {
-              //   let regex: RegExp = /\+?([ -]?\d+)+|\(\d+\)([ -]\d+) /g;
-              //   let regexresult: boolean = regex.test(phoneNumber);
-
-              //   let phoneNumberLength = phoneNumber.length;
-              //   if (phoneNumberLength < 11 || phoneNumberLength > 15) {
-              //     setErrorInvalidPattern("invisible");
-              //     setErrorInvalidLength("visible");
-              //   } else {
-              //     if (regexresult === false) {
-              //       setErrorInvalidLength("invisible");
-              //       setErrorInvalidPattern("visible");
-              //     } else {
-              //       setErrorInvalidLength("invisible");
-              //       setErrorInvalidPattern("invisible");
-              updateContactList(props.id, namemodal, phoneNumbermodal);
-              updateSearchContactList(props.id, namemodal, phoneNumbermodal);
-              UpdateCookies([...props.nonSearchContactList]);
-              props.setShowModalContactUpdate("invisible");
-              //     }
-              //   }
+              ErrorCheckname(namemodal);
+              ErrorCheckphoneNumber(phoneNumbermodal);
+              console.log("clicked");
+              console.log(phoneNumbermodal);
+              console.log(namemodal);
+              console.log(errorCheckNamePass);
+              console.log(errorCheckNumberPass);
+              if (
+                errorCheckNamePass === true &&
+                errorCheckNumberPass === true
+              ) {
+                setErrorInvalidLength("invisible");
+                setErrorInvalidPattern("invisible");
+                updateContactList(props.id, namemodal, phoneNumbermodal);
+                updateSearchContactList(props.id, namemodal, phoneNumbermodal);
+                UpdateCookies([...props.nonSearchContactList]);
+                props.setShowModalContactUpdate("invisible");
+                //     }
+                //   }
+              }
             }}
           >
             <h3 className="mt-1  ml-3 mr-4 mb-1 text-xs"> Update Contact</h3>
@@ -134,5 +184,4 @@ function ModalUpdateData(props: UpdateContactProps) {
     </div>
   );
 }
-
 export default ModalUpdateData;
